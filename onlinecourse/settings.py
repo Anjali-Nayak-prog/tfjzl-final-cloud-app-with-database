826c42903c76e5bb5d68be039d3275e2f21cdc91
Anjali-Nayak-prog <anjaliknayak143@gmail.com>
2026-09-22 19:24:05 +0530

    Add Bootstrap course details template

--- onlinecourse/settings.py
+++ onlinecourse/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'onlinecourse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'onlinecourse' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- onlinecourseapp/apps.py
+++ onlinecourseapp/apps.py
@@ -2,4 +2,6 @@ from django.apps import AppConfig
 
 
 class OnlinecourseappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'onlinecourseapp'
+    verbose_name = 'OnlineCourse'
